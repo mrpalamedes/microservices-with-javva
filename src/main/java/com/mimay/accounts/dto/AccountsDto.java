4e516a94a4fa28ad3371c20895ee1487af73ca6c
zidan <zidan.taufan@binus.ac.id>
2024-07-24 17:42:57 +0700

    feat: enhanching documentation

--- src/main/java/com/mimay/accounts/dto/AccountsDto.java
+++ src/main/java/com/mimay/accounts/dto/AccountsDto.java
@@ -1,20 +1,34 @@
 package com.mimay.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 @Data
 public class AccountsDto {
 
     @NotEmpty(message = "Account number cannot be a null or empty.")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits.")
+    @Schema(
+            description = "Account Number", example = "3454433243"
+    )
     private Long accountNumber;
 
+    @Schema(
+            description = "Account Type", example = "Savings, Credit"
+    )
     @NotEmpty(message = "Account type cannot be a null or empty.")
     private String accountType;
 
+    @Schema(
+            description = "Branch Address", example = "Komplek Senayan Residence"
+    )
     @NotEmpty(message = "Branch address cannot be a null or empty.")
     private String branchAddress;
 }
--- src/main/java/com/mimay/accounts/dto/CustomersDto.java
+++ src/main/java/com/mimay/accounts/dto/CustomersDto.java
@@ -1,6 +1,7 @@
 package com.mimay.accounts.dto;
 
 import com.mimay.accounts.entity.Accounts;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -8,16 +9,29 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
 public class CustomersDto {
 
+    @Schema(
+            description = "Name of the customer", example = "John Doe"
+    )
     @NotEmpty(message = "Name cannot be a null or empty.")
     @Size(min = 5, max = 30, message = "Name length should be between 5 and 30 characters.")
     private String name;
 
+    @Schema(
+            description = "Email address of the customer", example = "john@mail.co.id"
+    )
     @NotEmpty(message = "Email cannot be a null or empty.")
     @Email(message = "Email address should be a valid format.")
     private String email;
 
+    @Schema(
+            description = "Mobile Number of the customer", example = "9345432123"
+    )
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits.")
     private String mobileNumber;
 
