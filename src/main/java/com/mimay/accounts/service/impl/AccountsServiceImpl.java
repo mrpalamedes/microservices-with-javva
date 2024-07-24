21d25b333d40212862a124338a0fa09bdf93ff18
zidan <zidan.taufan@binus.ac.id>
2024-07-24 16:28:33 +0700

    feat: added api documentation dependency using springdoc openapi

--- src/main/java/com/mimay/accounts/service/impl/AccountsServiceImpl.java
+++ src/main/java/com/mimay/accounts/service/impl/AccountsServiceImpl.java
@@ -33,8 +33,6 @@ public class AccountsServiceImpl implements IAccountsService {
         if(optionalCustomer.isPresent()){
             throw new CustomerAlreadyExistsException("Customer already exists with given mobileNumber " + customersDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -47,8 +45,6 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 
