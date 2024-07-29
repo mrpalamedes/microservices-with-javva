49c888141241c182f73151479535d450ba906098
mrpalamedes <zidantaufan44@gmail.com>
2024-07-29 16:48:47 +0700

    build: add BaseEntity, Cards entity, and CardsRepository

---
+++ cards/src/main/java/com/mimay/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.mimay.cards.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mimay.cards.entity.Cards;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long>{
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+}
